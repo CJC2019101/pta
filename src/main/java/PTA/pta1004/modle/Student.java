5afd9ccc196b5c18326be1919e446d193bb0b169
cjf <2196124754@qq.com>
2020-01-07 18:04:03 +0800

    foreach 修改

--- src/main/java/PTA/pta1004/modle/Student.java
+++ src/main/java/PTA/pta1004/modle/Student.java
@@ -1,16 +1,42 @@
 package PTA.pta1004.modle;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * @author JianfeiChen
  * @date 2020/1/7 10:19
- * @Description PTA.pta1004.modle 学生类
+ * @Description foreach遍历Java集合删除元素 抛“CurrentModificationException” 并发修改异常
  */
-public class Student implements Comparable<Student> {
-
-    private String name;
-    private Double score; // 成绩
-    @Override
-    public int compareTo(Student that) {
-        return 0;
+public class Student{
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        set.add(5);
+        set.add(6);
+        set.add(7);
+        set.add(8);
+        set.add(9);
+        set.add(10);
+        set2.add(2);
+        set2.add(3);
+        set2.add(4);
+//        for (Integer integer : set) {
+//            if (set2.contains(integer)){
+//                set.remove(integer);
+//            }
+//        }
+        Iterator<Integer> integerIterator = set.iterator();
+        while (integerIterator.hasNext()){
+            if (set2.contains(integerIterator.next())){
+                integerIterator.remove(); // 删除元素
+            }
+        }
+        System.out.println("set = " + set);
     }
 }
